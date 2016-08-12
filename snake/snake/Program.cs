99a845626ecf5738cc329fd219efedc42428e063
dr7ver <bm.driver@outlook.com>
2016-08-12 11:06:13 +0300

    class zmeika

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,23 +10,21 @@ namespace snake
         static void Main(string[] args)
         {
 
-
+            Console.SetBufferSize(80,25);
 
             HorizLine line1 = new HorizLine(0, 78, 0, '-');
             line1.Drow();
-
             vert line2 = new vert(0, 24, 0, ':');
             line2.Drow();
-
             HorizLine line3 = new HorizLine(0, 78, 24, '-');
             line3.Drow();
-
             vert line4 = new vert(0, 24, 78, ':');
             line4.Drow();
 
 
-
-
+            Point  p = new Point(4,5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
 
             Console.ReadLine();
 
--- snake/snake/snake.cs
+++ snake/snake/snake.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Text;
 
 namespace snake
 {
-    class Class2
+    class Snake : Class1
     {
+        public Snake(Point tail, int lenght, Direction direction)
+        {
+            plist = new List<Point>();
+            for (int i = 0; i < lenght; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                plist.Add(p);
+            }
+        }
     }
 }
